a20dfee57a7c77db1221ac1ca2848701f134000b
qoribulmujib <muhamadqoribulmujib@gmail.com>
2024-12-05 11:34:24 +0700

    ✨ feat: tugas hari ke-3

---
+++ components/header/index.js
@@ -0,0 +1,13 @@
+import Menu from "../menu";
+
+import { withAuth } from "../with-auth";
+
+function Header() {
+  return (
+    <div>
+      <Menu />
+    </div>
+  );
+}
+
+export default withAuth(Header);
