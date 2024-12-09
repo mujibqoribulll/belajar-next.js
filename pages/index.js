fa14e602915bc2cf3364bdf841973077e223dd7e
qoribulmujib <muhamadqoribulmujib@gmail.com>
2024-12-09 21:16:22 +0700

    🎉 feat: tugas hari ke-5

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,11 @@
-import Head from "next/head";
-import Image from "next/image";
-import localFont from "next/font/local";
-import styles from "@/styles/Home.module.css";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
-import Content from "@/components/content";
-
+import Layout from "@/components/layout";
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <Content />
-      <Footer />
+      <Layout metaTitle={"next js"} metaDescription={"belajar next js"}>
+        Content
+      </Layout>
     </>
   );
 }
---
+++ pages/profile.js
@@ -0,0 +1,9 @@
+import Layout from "@/components/layout";
+
+export default function Profile() {
+  return (
+    <Layout>
+      <p>profile</p>
+    </Layout>
+  );
+}
