d479f5cd7a295860d37948435f4c9ce489608f75
qoribulmujib <muhamadqoribulmujib@gmail.com>
2024-12-11 00:02:59 +0700

    ✨ feat: tugas hari ke-7

--- components/header/index.js
+++ components/header/index.js
@@ -1,12 +1,23 @@
 import Menu from "../menu";
-import styles from './styles.module.css'
+import styles from "./styles.module.css";
 
 import { withAuth } from "../with-auth";
+import Link from "next/link";
 
 function Header() {
   return (
     <div className={styles.header}>
-      header
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/users">Users</Link>
+        </li>
+        <li>
+          <Link href="/prodile">Profile</Link>
+        </li>
+      </ul>
     </div>
   );
 }
---
+++ pages/users/detail.js
@@ -0,0 +1,9 @@
+import Layout from "@/components/layout";
+
+export default function UserDetail() {
+  return (
+    <Layout>
+      <p>UserDetail</p>
+    </Layout>
+  );
+}
