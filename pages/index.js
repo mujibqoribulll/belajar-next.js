fdb57cdd1e488d9e457a3cb3af35cb138b277f76
qoribulmujib <muhamadqoribulmujib@gmail.com>
2024-12-12 17:24:12 +0700

    feat: tugas hari ke-9

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,21 @@
 import Layout from "@/components/layout";
+import { useEffect } from "react";
 
 export default function Home() {
+  async function fetchAlldata() {
+    try {
+      let response = await fetch("api/hello");
+      let result = await response.json();
+      console.log('result', result)
+    } catch (error) {
+      console.log("error", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchAlldata();
+  }, []);
+
   return (
     <>
       <Layout metaTitle={"Home"} metaDescription={"belajar next js"}>
